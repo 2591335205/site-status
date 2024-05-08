e8cef8259de9af551ba7d84ee64e528eaee2bcc0
Chen Wanglong <128337660+2591335205@users.noreply.github.com>
2024-05-08 12:24:03 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
         <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.aimicroiot.com/" text="Humility" />
+          <CustomLink to="https://www.aimicroiot.com/" text=":-)" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
